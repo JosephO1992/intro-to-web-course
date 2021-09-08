19cc2c3543f20305bb42f830f165f0e006446248
Joe <josephraoreilly@gmail.com>
2021-09-08 21:36:18 +0100

    nav now looping through array

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,39 @@
     <h1 class="flex items-center">Intro to Web Course</h1>
   </div>
   <div class="flex flex-row justify-between">
-    <div class="flex flex-col w-48">
-      <router-link to="/" class="h-8 flex items-center">Home</router-link>
-      <router-link to="/about">Lesson 1</router-link>
-      <router-link to="/about">Lesson 2</router-link>
-      <router-link to="/about">Lesson 3</router-link>
-      <router-link to="/about">Lesson 4</router-link>
-    </div>
+      <ul class="flex flex-col w-48">
+        <router-link v-for="(link, index) in links" :key="index" :to="link.route">
+          {{link.title}}  
+        </router-link>
+      </ul>
     <router-view class="w-full"/>
   </div>
 </div>
 </template>
 <script>
 export default {
-  
+  data () {
+    return {
+      links: [
+        {
+          title: 'Home',
+          route: '/'
+        },
+        {
+          title: 'Lesson 1',
+          route: '/lesson-1'
+        },
+        {
+          title: 'Lesson 2',
+          route: '/lesson-2'
+        },
+        {
+          title: 'Lesson 3',
+          route: '/lesson-3'
+        }
+      ]
+      
+    }
+  }
 }
 </script>
