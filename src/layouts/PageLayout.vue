5c6be51d9c28db8bf2ebf36805b9cd2ec4a41768
JosephO1992 <josephraoreilly@gmail.com>
2021-11-02 13:28:42 +0000

    now using a page layout

--- src/layouts/PageLayout.vue
+++ src/layouts/PageLayout.vue
@@ -1,20 +1,12 @@
 <template>
-       <div class="flex flex-col flex-grow h-full">
-        <div class="flex flex-col p-5 text-xl flex-grow">
-            <h2 class="font-bold text-left mb-8 underline border-b-2 pb-4 border-gray-800">
-                {{title}}
-            </h2>
-        
-        </div>
+  <div class="flex flex-col flex-grow h-full">
+    <div class="flex flex-col p-5 text-xl flex-grow bg-gray-100">
+      <slot></slot>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    props: {
-        title: {
-            type: String,
-            required: true
-        }
-    }
-}
+  name: "PageLayout",
+};
 </script>
--- src/views/Lesson2.vue
+++ src/views/Lesson2.vue
@@ -1,16 +1,62 @@
 <template>
-      <div class="flex flex-col flex-grow h-full">
-        <div class="flex flex-col p-5 text-xl flex-grow">
-            <h2 class="font-bold text-left mb-8 underline border-b-2 pb-4 border-gray-800">Lesson 2 - Let's do some styling</h2>
-        <p class="text-left mb-4">Hi there! 👋🏻 My name is Joe O'Reilly, I'm a Software Devloper. I've been developing in my own time for a while now, and in the last few months it's become my full time job.</p>
-        <p class="text-left mb-4">I'd like to share my passion with you, and hopefully you can learn something new and exciting.</p>
-    
-            <p class="text-left mb-4">
-                🙏🏻 Welcome to your introduction into Web Development.
-            </p>
+  <page-layout>
+    <h2 class="font-bold mb-8 underline border-b-2 pb-4 border-gray-800">
+      Lesson 2 - Let's make some things
+    </h2>
+    <h3 class="mb-4">Exercise 1 - Start your webpage</h3>
+    <p class="text-left mb-4">
+      I want you to make a basic homepage (we will style it later).
+    </p>
+    <p class="text-left mb-4">This should include:</p>
+    <ol class="flex flex-col ml-8">
+      <li class="mb-6 text-lg flex flex-col">
+        <p class="mb-2">
+          A header tag <code>h1</code> with the name of your homepage.
+        </p>
+      </li>
+      <li class="mb-6 text-lg flex flex-col">
+        <p class="mb-2">
+          A p tag <code>p</code> with a brief introduction about yourself.
+        </p>
+      </li>
+      <li class="mb-6 text-lg flex flex-col">
+        <p class="mb-2">
+          2 lists, one using <code>ul</code> and another using <code>ol</code>.
+          Ideas for this could be your favorite movies, foods, sports, football
+          players, games, anything you like!
+        </p>
+      </li>
+      <li class="mb-6 text-lg flex flex-col">
+        <p class="mb-2">
+          Finally, an <code>a</code> which can link to your favorite website,
+          you can make this related to your list if you like.
+        </p>
+      </li>
+    </ol>
 
-            <p class="text-left mb-4">The aim of the course is to:</p>
-            
-        </div>
-    </div>
+    <p class="text-left mb-4">
+      Now, this will be the start of your web development journey, you may have
+      to google your way through it, and that's fine. Some other useful
+      resources are:
+      <a
+        href="https://developer.mozilla.org/en-US/"
+        class="text-gray-500 hover:text-blue-500"
+        >Mozilla Developer Docs</a
+      >
+      and
+      <a
+        href="https://www.w3schools.com/html/default.asp"
+        class="text-gray-500 hover:text-blue-500"
+        >W3Schools</a
+      >.
+    </p>
+  </page-layout>
 </template>
+<script>
+import PageLayout from "../layouts/PageLayout.vue";
+export default {
+  components: {
+    PageLayout,
+  },
+};
+</script>
