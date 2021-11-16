8819858f96120c6923d33e0be4bc95376593ed3e
JosephO1992 <josephraoreilly@gmail.com>
2021-11-16 13:54:53 +0000

    changed title back

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="text-center flex items-center align-middle flex-shrink-0 h-20 bg-gray-800 text-gray-200">
-		<h1 class="ml-10 text-2xl">Intro to Git Development</h1>
+		<h1 class="ml-10 text-2xl">Intro to Web Development</h1>
 	</div>
 </template>
 <script>
